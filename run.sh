c868b41ab2fe5eae30c244fda9fc1278830014b0
omogr <omogrus@ya.ru>
2026-07-11 20:41:43 +0300

    initial commit
    Signed-off-by: omogr <omogrus@ya.ru>

--- run.sh
+++ run.sh
@@ -2,48 +2,48 @@
 
 # эксперимент - что будет, если обрабатывать более крупными кусками (ничего интересного, всё то же самое)
 
-# python merge_sentences.py src/GOLD_results.txt -o dst/pattern.json -m 500
+# python merge_sentences.py gold/pattern.txt -o output/pattern.json -m 500
 
 # разбиваем текстовый файл (образец с ручной разметкой) на предложения по строкам
 
-python split_sentences_by_lines.py src/GOLD_results.txt -o dst/pattern.json
+python split_sentences_by_lines.py gold/pattern.txt -o output/pattern.json
 
 # Добавляем пословную разметку
 
-python extract_gold_accentuation.py dst/pattern.json -o dst/results/GOLD_results.json
+python extract_gold_accentuation.py output/pattern.json -o output/lib/GOLD_results.json
 
 # размечаем ударения библиотекой ruaccent_turbo
 
-# python run_accentuator.py ruaccent_turbo dst/results/GOLD_results.json -o dst/tmp
+python run_accentuator.py ruaccent_turbo output/lib/GOLD_results.json -o output/raw
 
 # размечаем ударения библиотекой silero_stress
 
-# python run_accentuator.py silero_stress dst/results/GOLD_results.json -o dst/tmp
+python run_accentuator.py silero_stress output/lib/GOLD_results.json -o output/raw
 
 # размечаем ударения библиотекой accent_engine
 
-python run_accentuator.py accent_engine dst/results/GOLD_results.json -o dst/results/
+python run_accentuator.py accent_engine output/lib/GOLD_results.json -o output/lib/
 
 # размечаем ударения библиотекой llm_enhancer
 
-# python run_accentuator.py llm_enhancer dst/GOLD_results.json -o dst/results/
+# python run_accentuator.py llm_enhancer output/GOLD_results.json -o output/lib/
 
 # размечаем ударения библиотекой wiki_enhancer
 
-python run_accentuator.py wiki_enhancer dst/results/GOLD_results.json -o dst/results/
+python run_accentuator.py wiki_enhancer output/lib/GOLD_results.json -o output/lib/
 
 # Добавляем пословную разметку к результатам ruaccent_turbo
 
-#python extract_word_accentuation.py dst/tmp/ruaccent_turbo_results.json -o dst/results/ruaccent_turbo_results.json
+python extract_word_accentuation.py output/raw/ruaccent_turbo_results.json -o output/lib/ruaccent_turbo_results.json
 
 # Добавляем пословную разметку к результатам silero_stress
 
-#python extract_word_accentuation.py dst/tmp/silero_stress_results.json -o dst/results/silero_stress_results.json
+python extract_word_accentuation.py output/raw/silero_stress_results.json -o output/lib/silero_stress_results.json
 
 # Считаем ошибки
 
-python compare_accentuators.py dst/results -o dst/comparison_results.json
+python compare_accentuators.py output/lib -o output/comparison.json
 
 # Формируем отчет
 
-python generate_report.py dst/comparison_results.json dst/report.md
+python generate_report.py output/comparison.json output/report.md
